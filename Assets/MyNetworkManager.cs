0b935f3401baa4ef8d4086328ca07fa8d12c307d
Eran <eran.lab.mail@gmail.com>
2020-09-07 21:06:09 +0300

    First Commit

---
+++ Assets/MyNetworkManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using TMPro;
+
+public class MyNetworkManager : NetworkManager
+{
+    [SerializeField] private TextMeshProUGUI tmpro;
+    [SerializeField] private List<string> clientNames;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/myHUD.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Mirror;
+
+public class myHUD : NetworkBehaviour
+{
+    [SerializeField] private TextMeshProUGUI tmpro;
+    [SerializeField] private List<string> clientNames;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        tmpro = gameObject.GetComponentInChildren<TextMeshProUGUI>();
+
+        
+        clientNames = new List<string>();
+        tmpro.text = "Connected Clients:";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [Server]
+    void OnClientsUpdated(string clientname, bool connected)
+    {
+        clientNames.Add(clientname);
+        tmpro.text = tmpro.text += " " + clientname;
+    }
+}
